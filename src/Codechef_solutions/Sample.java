be72c7f94fa5deb5418141c0cbb0b4b1c4c0e2d9
Vishal Bhutekar <127893332+vishal-bhutekar21@users.noreply.github.com>
2025-12-13 23:47:41 +0530

    solved two sum using hashmap

--- src/Codechef_solutions/Sample.java
+++ src/Codechef_solutions/Sample.java
@@ -1,4 +1,14 @@
 package Codechef_solutions;
 
-public class Sample {
+import java.util.Scanner;
+
+public class Sample    {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char first, second;
+        first = scanner.next().charAt(0);
+        second = scanner.next().charAt(0);
+        System.out.println(first + second);
+        scanner.close();
+    }
 }
--- src/conditional_loops_03/Two_sum.java
+++ src/conditional_loops_03/Two_sum.java
@@ -1,6 +1,7 @@
 package conditional_loops_03;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Two_sum {
@@ -13,17 +14,36 @@ public class Two_sum {
         System.out.println("solution of two sum problem is:"+ Arrays.toString(solutiontwosum(nums,target)));
     }
 
-    private static int []  solutiontwosum(int[] nums, int target) {
+//    private static int []  solutiontwosum(int[] nums, int target) {
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i+1; j <nums.length ; j++) {
+//                if (target==nums[i]+nums[j]){
+//                    return new int[]{i,j};
+//
+//                }
+//                                                  uses approach O(n2)
+//            }
+//        }
+//        return new int[]{};
+//    }
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j <nums.length ; j++) {
-                if (target==nums[i]+nums[j]){
-                    return new int[]{i,j};
 
-                }
+    private static int [] solutiontwosum(int nums[],int target){
 
+        HashMap<Integer,Integer>map=new HashMap<>();
+
+        for (int i = 0; i <nums.length  ; i++) {
+
+            int c=target-nums[i];
+
+            if (map.containsKey(c)){
+                return new int[]{map.get(c),i};
             }
+            map.put(nums[i],i );
+
         }
-        return new int[]{};
+
+        return new int []{};
     }
 }
