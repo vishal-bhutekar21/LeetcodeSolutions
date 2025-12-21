6e247987d568c5d8b199528484d4355c9b1152cf
vishal-bhutekar21 <vishalbhutekar33772@gmail.com>
2025-12-21 20:22:21 +0530

    error solved leetcode 1672 easy richest person wealth

--- src/Array_04/richestcustomer_1672.java
+++ src/Array_04/richestcustomer_1672.java
@@ -1,15 +1,20 @@
 package Array_04;
 
-import java.util.Scanner;
-
 public class richestcustomer_1672 {
-    public static void main(String ...args){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
 
-        int arr[][]=push {{1,2,3,4,},{1,2,4,5,6}};
+        int[][] arr = {
+                {1, 2, 3, 4},
+                {1, 2, 4, 5, 6}
+        };
 
+        System.out.println(
+                "The maximum sum of the richest customer wealth is "
+                        + maximumWealth(arr)
+        );
     }
-    static public int maximumWealth(int[][] accounts) {
+
+    public static int maximumWealth(int[][] accounts) {
         int maxWealth = 0;
 
         for (int i = 0; i < accounts.length; i++) {
