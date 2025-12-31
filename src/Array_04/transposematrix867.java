6e70210f3cf4575a570b21d1e29d2bc5e0e28cb7
vishal-bhutekar21 <vishalbhutekar33772@gmail.com>
2025-12-31 23:27:26 +0530

    solved two leetcode easy problem transposematrix867 where we need to convert the rows to column and columns to row

---
+++ src/Array_04/transposematrix867.java
@@ -0,0 +1,35 @@
+package Array_04;
+
+public class transposematrix867 {
+
+    public static int[][] transpose(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] result = new int[cols][rows];
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                result[c][r] = matrix[r][c];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        int[][] ans = transpose(matrix);
+
+        for (int i = 0; i < ans.length; i++) {
+            for (int j = 0; j < ans[0].length; j++) {
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
