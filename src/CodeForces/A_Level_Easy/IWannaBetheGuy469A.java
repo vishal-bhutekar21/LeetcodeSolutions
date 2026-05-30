9ae4034e9b0ce42e85b1e0136653a3090288bb0c
vishal-bhutekar21 <vishalbhutekar33772@gmail.com>
2026-05-31 00:22:56 +0530

    solved one IWannaBetheGuy469A problem

---
+++ src/CodeForces/A_Level_Easy/IWannaBetheGuy469A.java
@@ -0,0 +1,30 @@
+package CodeForces.A_Level_Easy;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class IWannaBetheGuy469A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        HashSet<Integer> levels = new HashSet<>();
+
+        int p = sc.nextInt();
+        for (int i = 0; i < p; i++) {
+            levels.add(sc.nextInt());
+        }
+
+        int q = sc.nextInt();
+        for (int i = 0; i < q; i++) {
+            levels.add(sc.nextInt());
+        }
+
+        if (levels.size() == n) {
+            System.out.println("I become the guy.");
+        } else {
+            System.out.println("Oh, my keyboard!");
+        }
+    }
+}
