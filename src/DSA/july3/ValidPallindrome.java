bbede2a9af187a89a39c3601cdf93ab9e7239a4e
vishal-bhutekar21 <vishalbhutekar33772@gmail.com>
2026-07-04 00:21:19 +0530

    solved one valid palindrom problem

--- src/DSA/july3/ValidPallindrome.java
+++ src/DSA/july3/ValidPallindrome.java
@@ -4,6 +4,36 @@ import java.util.Scanner;
 // checks if the string is pallindrome with remove the spaces and the symbos and he digits
 
 public class ValidPallindrome {
+    class Solution {
+        static boolean isPalindrome2(String s) {
+
+            int left = 0;
+            int right = s.length() - 1;
+
+            while (left < right) {
+
+
+                while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                    left++;
+                }
+
+                while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                    right--;
+                }
+
+
+                if (Character.toLowerCase(s.charAt(left)) !=
+                        Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
 
     static boolean isPalindrome(String s) {
         String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
