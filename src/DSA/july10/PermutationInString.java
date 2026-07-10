dd1f089e75a8a9356e75ce4b78daa8cfb8cc895d
vishal-bhutekar21 <vishalbhutekar33772@gmail.com>
2026-07-11 00:51:50 +0530

    solved using the chatgpt

--- src/DSA/july10/PermutationInString.java
+++ src/DSA/july10/PermutationInString.java
@@ -3,17 +3,58 @@ package DSA.july10;
 import java.util.Scanner;
 
 public class PermutationInString {
+
     static boolean checkInclusion(String s1, String s2) {
 
+        if (s1.length() > s2.length()) {
+            return false;
+        }
 
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        int k = s1.length();
+
+        for (int i = 0; i < k; i++) {
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
+        }
+
+        if (matches(freq1, freq2)) {
+            return true;
+        }
+
+        for (int i = k; i < s2.length(); i++) {
+
+            freq2[s2.charAt(i) - 'a']++;
+
+            freq2[s2.charAt(i - k) - 'a']--;
+
+            if (matches(freq1, freq2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static boolean matches(int[] a, int[] b) {
+
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s1, s2;
 
-        s2 = sc.next();
-        s1 = sc.next();
+        Scanner sc = new Scanner(System.in);
+
+        String s2 = sc.next();
+        String s1 = sc.next();
 
         System.out.println("contains or not " + checkInclusion(s1, s2));
     }
